f5d424b03f11ebca6c86b1800b0f4af828a6a002
Satoshi Ogata <satosystems@gmail.com>
2020-11-02 12:11:22 +0900

    update macrm.rb for v1.0.0.4

--- macrm.rb
+++ macrm.rb
@@ -1,8 +1,8 @@
 class Macrm < Formula
   desc "This program is a replacement of macOS's rm command."
   homepage "https://github.com/satosystems/macrm"
-  url "https://github.com/satosystems/macrm/releases/download/v1.0.0.3/macrm"
-  sha256 "06605fbc72247c60c59d5611e2362ce6f8d342b08131a40e74d36e16f5f443d7"
+  url "https://github.com/satosystems/macrm/releases/download/v1.0.0.4/macrm"
+  sha256 "ab8808256e2af1ee7dd9e20773299a70d8f826484dde65013c80163075c2cfe9"
   license "BSD-3-Clause License"
 
   def install
