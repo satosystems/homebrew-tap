a8ec1e101bdd8b9c21d3dca92a48c078b5cc9716
Satoshi Ogata <satosystems@gmail.com>
2022-03-19 18:46:25 +0900

    Update ppap.rb for v1.1.1

--- ppap.rb
+++ ppap.rb
@@ -1,14 +1,14 @@
 class Ppap < Formula
   desc "This is a CLI program that creates a ZIP file with a password."
   homepage "https://github.com/satosystems/ppap"
-  url "https://github.com/satosystems/ppap/releases/download/v1.1.0/ppap-v1.1.0.tar.gz"
-  sha256 "5fe9bceb0a9a38309199149ef71181982634852d6566591480027a483d3b2078"
+  url "https://github.com/satosystems/ppap/releases/download/v1.1.1/ppap-v1.1.1.tar.gz"
+  sha256 "8d4352ae7931f0f1ce13e5f5d87bdfea90eadee505de859790da91971d964e8a"
   license "Apache License"
   head "https://github.com/satosystems/ppap.git"
 
   def install
     bin.install "ppap"
-    bin.install "ppap-v1.1.0.jar"
+    libexec.install "ppap-v1.1.1.jar"
   end
 
   test do
